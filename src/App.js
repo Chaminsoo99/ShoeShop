a83d5205e358d41cce3ff3920d2cf8fa1ab3c57c
차민수 <12chd@naver.com>
2024-02-11 05:12:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container} from 'react-bootstrap';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">ShoeShop</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Cart</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    <div className='main-bg'></div>
+
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + '/img/shoes1.jpg'} width='80%' /> 
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div>
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + '/img/shoes2.jpg'} width='80%' /> 
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div>
+        <div className="col-md-4">
+          <img src={process.env.PUBLIC_URL + '/img/shoes3.jpg'} width='80%' /> 
+          <h4>상품명</h4>
+          <p>상품정보</p>
+        </div> 
+      </div>
+    </div>
+
+
+
     </div>
   );
 }
